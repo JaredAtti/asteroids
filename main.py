6850e257f249063a2dedd06eb81d5198b32a5d99
JaredAtti <jaredatti@gmail.com>
2025-01-07 12:29:24 -0500

    add updateable and drawable groups for sprites

--- main.py
+++ main.py
@@ -6,19 +6,29 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
-    dt = 0
+    
+    updateable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+
+    Player.containers = (updateable, drawable)
 
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         
-        player.update(dt)
-            
+        for u in updateable:
+            u.update(dt)
+
         screen.fill("black")
-        player.draw(screen)
+
+        for d in drawable:
+            d.draw(screen)
+        
         pygame.display.flip()
 
         # limit the framerate to 60 FPS
